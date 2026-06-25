bb651ce98bcdd52d2f616c5ad4bcdd806dbc97d7
Rohtash <rohtashchauhan@sagoon.com>
2026-06-25 13:46:57 +0530

    Revert "fix: add ambient type declaration for mammoth module"
    This reverts commit 525d07d8196affe7d9343ece3e67fb74eafced7a.

--- services/api/src/mammoth.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'mammoth' {
-  interface Result {
-    value: string;
-    messages: unknown[];
-  }
-  export function extractRawText(input: { buffer: Buffer }): Promise<Result>;
-  export function convertToHtml(input: { buffer: Buffer }): Promise<Result>;
-}
